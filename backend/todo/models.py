186e48efe95f0dbadf04d35c31121688aa4509c1
Pedro <pedro_rga@hotmail.com>
2021-06-02 12:41:42 -0300

    simple model

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+# from django.core.validators import MaxValueValidator
 
 # Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    # models.DecimalField(validators=[MaxValueValidator(100)])
+
+    def _str_(self):
+        return self.title
